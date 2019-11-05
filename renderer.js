5edd6fa63e8e9eead6f4df8904b0ba2727f568b4
Szymon Drawski <draszy1@gmail.com>
2019-11-05 17:39:11 +0100

    bootstrap applied

--- renderer.js
+++ renderer.js
@@ -4,3 +4,21 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+//const {Plotly} = require("plotly.js-dist");
+
+let trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    type: 'scatter',
+};
+
+let trace2 = {
+    x: [1, 2, 3, 4],
+    y: [16, 5, 11, 9],
+    type: 'scatter'
+};
+
+let data = [trace1, trace2];
+
+Plotly.newPlot('graph-canvas', data, {}, {showSendToCloud: true});
